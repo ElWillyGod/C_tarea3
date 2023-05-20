47c6dfd172bc92970884b5cd332f9a9d72c0ab98
ElWillyGod <manuelantognazza@gmail.com>
2023-05-20 20:49:27 -0300

    mejoraAplicaiones

--- src/aplicaciones.cpp
+++ src/aplicaciones.cpp
@@ -7,14 +7,13 @@ TPilaPersona menoresQueElResto(TPersonasLDE lista)
 
     while (cantidadTPersonasLDE(lista) > 0)
     {
-        TPersona inicio = obtenerInicioDeTPersonasLDE(lista);
 
-        while ((cantidadEnTPilaPersona(menores) > 0) && (edadTPersona(cimaDeTPilaPersona(menores)) >= edadTPersona(inicio)))
+        while ((cantidadEnTPilaPersona(menores) > 0) && (edadTPersona(cimaDeTPilaPersona(menores)) > edadTPersona(obtenerInicioDeTPersonasLDE(lista))))
         {
             desapilarDeTPilaPersona(menores);
         }
 
-        apilarEnTPilaPersona(menores, inicio);
+        apilarEnTPilaPersona(menores, obtenerInicioDeTPersonasLDE(lista));
         eliminarInicioTPersonasLDE(lista);
     }
 
@@ -24,58 +23,26 @@ TPilaPersona menoresQueElResto(TPersonasLDE lista)
 bool sumaPares(nat k, TConjuntoIds c)
 {
 
-    bool hash[cantMaxTConjuntoIds(c) + 1];
+    bool aux[cantMaxTConjuntoIds(c) + 1];
 
     for (nat i = 1; i <= cantMaxTConjuntoIds(c); i++)
     {
-        hash[i] = false;
+        aux[i] = false;
     }
 
     for (nat i = 1; i <= cantMaxTConjuntoIds(c); i++)
     {
+
         if (perteneceTConjuntoIds(i, c))
         {
-
-            nat complemento = k - i;
-
-            if (complemento > 0 && complemento <= cantMaxTConjuntoIds(c) && hash[complemento])
+            if (k - i > 0 && k - i <= cantMaxTConjuntoIds(c) && aux[k - i])
             {
                 return true;
             }
 
-            hash[i] = true;
+            aux[i] = true;
         }
     }
 
     return false;
 }
-/*if (k > conjunto.cantMax || k < 1) {
-        return false;
-    }
-
-    for (int i = 1; i <= k / 2; i++) {
-        if (conjunto.conjunto[i] && conjunto.conjunto[k - i]) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-    TConjuntoIds comp = crearTConjuntoIds(cantMaxTConjuntoIds(c));
-    nat i =1;
-
-    while (cardinalTConjuntoIds(c) > 0)
-    {
-        if(perteneceTConjuntoIds(i,comp)){
-            liberarTConjuntoIds(comp);
-            return true;
-        }else{
-            insertarTConjuntoIds(k-i, comp);
-        }
-
-    }
-
-    liberarTConjuntoIds(comp);
-    return false;
-*/
